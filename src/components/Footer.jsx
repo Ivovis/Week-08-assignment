b224852456539572d3ed6b00b13f21def3d8b05d
Christopher Hunn <christopher.hunn@gmail.com>
2025-06-20 14:25:01 +0100

    fix: spelling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <>
       <div className=" justify-items-center text-xs p-3 font-[family-name:var(--font-geist-sans)]">
-        <h1>&copy; Christoper Hunn 2025</h1>
+        <h1>&copy; Christopher Hunn 2025</h1>
       </div>
     </>
   );
